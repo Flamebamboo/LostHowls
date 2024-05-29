02564bd7b2c53766d5e62ac6733178959a6fb5f3
Flamebamboo <166450271+Flamebamboo@users.noreply.github.com>
2024-05-29 23:35:24 +1200

    Added more logic in bat script
    ONE LAST COMMIT 11:35pm before sleep. Added more new variables and logics to handle dog attacks.
    
    summary of changes:
    
    if its not dead, roaming is set to true, if its not taking damage and it will chase the dog. if its not chasing it will switch to roam state
    
    added some dead movement making it falls down, it will delete itself after 3 sec
    
    added hurt animation when taking damage
    
    Added knockback when damage are taken

--- bat.gd
+++ bat.gd
@@ -2,30 +2,49 @@ extends CharacterBody2D
 
 @export var SPEED = 30
 var dir: Vector2
-var bat_chase: bool
+var batChase: bool
 var dog: CharacterBody2D
-
+var health = 50
+var healthMax = 50
+var healthMin = 0
+var dead = false
+var takingDamage = false
+var isRoaming: bool
 func _ready():
-	bat_chase = true
-	
+	batChase = true
 	
 	
 func _process(delta):
+	
+	if is_on_floor() and dead:
+		await get_tree().create_timer(3.0).timeout
+		self.queue_free()
 	move(delta)
 	handle_animation()
-
+#bat logic
 func move(delta):
-	if bat_chase:
-		dog = Global.dogCharacter
-		velocity = position.direction_to(dog.position) * SPEED
-		dir.x = abs(velocity.x) / velocity.x
-	elif !bat_chase:
-		velocity += dir * SPEED * delta 
+	dog = Global.dogCharacter
+	if !dead:
+		isRoaming = true
+		if !takingDamage and batChase: 
+			dog = Global.dogCharacter
+			velocity = position.direction_to(dog.position) * SPEED
+			dir.x = abs(velocity.x) / velocity.x
+		elif takingDamage:
+			var knockbackDir = position.direction_to(dog.position) * -50
+			velocity = knockbackDir
+			
+			
+		else: #if not chase then roam
+			velocity += dir * SPEED * delta 
+	elif dead: #fall
+		velocity.y += 10 * delta
+		velocity.x = 0
 	move_and_slide()
 	
 func _on_timer_timeout():
 	$Timer.wait_time = choose([1.0, 1.5, 2])
-	if !bat_chase:
+	if !batChase:
 		dir = choose([Vector2.RIGHT, Vector2.LEFT, Vector2.UP, Vector2.DOWN])
 		
 		
@@ -35,9 +54,39 @@ func choose(array):
 
 func handle_animation():
 	var animatedSprite = $AnimatedSprite2D
-	animatedSprite.play("flying")
-	if dir.x == 1:
-		animatedSprite.flip_h = false
+	if !dead and !takingDamage:
+		animatedSprite.play("flying")
+		if dir.x == 1:
+			animatedSprite.flip_h = false
 		
-	if dir.x == -1:
-		animatedSprite.flip_h = true
+		if dir.x == -1:
+			animatedSprite.flip_h = true
+	elif !dead and takingDamage:
+		animatedSprite.play("hurt")
+		await get_tree().create_timer(0.8).timeout
+		takingDamage = false
+	elif dead and isRoaming:
+		isRoaming = false
+		animatedSprite.play("death")
+	
+	
+
+
+
+	
+
+
+func _on_bat_hitbox_area_entered(area):
+	if area == Global.dogDamageZone:
+		var damage = Global.dogDamageAmount
+		takeDamage(damage)
+
+func takeDamage(damage):
+	health -= damage
+	takingDamage = true
+	if health <= 0:
+		health = 0 
+		dead = true
+	print(str(self), "current health is ", health)
+
+		
